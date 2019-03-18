07c9461a86700ee7d17e0b3b9635af0c697bc51c
vietanhtran1996 <tranvietanh1996@gmail.com>
2019-03-18 22:30:28 +1300

    Tutorial wip

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../services/mock-heroes';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+}
---
+++ src/app/services/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from '../hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Iron Man'},
+    {id: 12, name: 'Captain Marvel'},
+    {id: 13, name: 'Captain America'},
+    {id: 14, name: 'Thor'},
+    {id: 15, name: 'Hulk'},
+    {id: 16, name: 'T\'challa'},
+];
