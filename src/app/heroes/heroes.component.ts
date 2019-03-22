e180181e92431f5bad89bfc61e4fa5dc09edeafc
vietanhtran1996 <tranvietanh1996@gmail.com>
2019-03-23 10:05:52 +1300

    WIP

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -16,10 +16,6 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
